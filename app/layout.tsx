51b0d4fac5e7f2f612c1d361051e5772ea663ced
Jason Savelli <jason.savelli@mention.com>
2024-03-23 14:41:11 +0100

    task: setup shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { cn } from './utils/cn'
+import { Inter as FontSans, Lora as FontSerif, Inconsolata as FontMono } from 'next/font/google'
 import { Header } from './header/header'
+import { cn } from '@/lib/utils'
 
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
+
+const fontSans = FontSans({ subsets: ['latin'], variable: '--font-sans' })
+const fontSerif = FontSerif({ subsets: ['latin'], variable: '--font-serif' })
+const fontMono = FontMono({ subsets: ['latin'], variable: '--font-mono' })
 
 export const metadata: Metadata = {
     title: 'Dictionary',
@@ -18,7 +21,14 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
-            <body className={cn('px-6 pb-16 pt-6', inter.className)}>
+            <body
+                className={cn(
+                    'min-h-[100dvh] px-6 pb-16 pt-6 font-sans antialiased',
+                    fontSans.variable,
+                    fontSerif.variable,
+                    fontMono.variable
+                )}
+            >
                 <Header />
 
                 <main>{children}</main>
