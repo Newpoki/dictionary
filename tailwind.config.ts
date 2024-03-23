f85bbfbb0ef728b83c484d99ba4233706be42721
Jason Savelli <jason.savelli@mention.com>
2024-03-23 14:04:13 +0100

    task: add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,64 @@ const config: Config = {
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
+        colors: {
+            black: '#000000',
+            white: '#FFFFFF',
+            red: {
+                500: '#FF5252',
+            },
+            purple: {
+                500: '#A445ED',
+                700: '#8F19E8',
+            },
+            grey: {
+                100: '#F4F4F4',
+                300: '#E9E9E9',
+                400: '#CECECE',
+                500: '#757575',
+                600: '#3A3A3A',
+                700: '#2D2D2D',
+                800: '#1F1F1F',
+                900: '#050505',
+            },
+        },
+        fontSize: {
+            'b-s': [
+                '14px',
+                {
+                    fontWeight: 500,
+                    lineHeight: '17px',
+                },
+            ],
+            'b-m': [
+                '18px',
+                {
+                    fontWeight: 500,
+                    lineHeight: '24px',
+                },
+            ],
+            'h-s': [
+                '20px',
+                {
+                    fontWeight: 500,
+                    lineHeight: '24px',
+                },
+            ],
+            'h-m': [
+                '24px',
+                {
+                    fontWeight: 500,
+                    lineHeight: '29px',
+                },
+            ],
+            'h-l': [
+                '64px',
+                {
+                    fontWeight: 700,
+                    lineHeight: '77px',
+                },
+            ],
+        },
         extend: {},
     },
     plugins: [],
