c22cb8900f0b16060946bd3bfd0fb07b73bf0553
Jason Savelli <jason.savelli@mention.com>
2024-03-24 13:22:22 +0100

    fix: svg wrong props

--- components/icons/chevron-down.tsx
+++ components/icons/chevron-down.tsx
@@ -1,7 +1,7 @@
 export const ChevronDown = (props: React.SVGProps<SVGSVGElement>) => {
     return (
         <svg {...props} viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 1L7 7L13 1" stroke="currentColor" stroke-width="1.5" />
+            <path d="M1 1L7 7L13 1" stroke="currentColor" strokeWidth="1.5" />
         </svg>
     )
 }
