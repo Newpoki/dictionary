98206a1765e8031566cb51e26f8fc14726a497be
Jason Savelli <jason.savelli@mention.com>
2024-03-24 14:31:30 +0100

    feat: dictionnary icon now redirect to home page

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -8,12 +8,18 @@ import { HeaderThemeSwitch } from './header-theme-switch'
 import { ChevronDown } from '@/components/icons/chevron-down'
 import { Dictionary } from '@/components/icons/dictionnary'
 import { HeaderSearch } from './header-search'
+import Link from 'next/link'
 
 export const Header = () => {
     return (
         <header className="flex flex-col gap-6 md:gap-[51.5px]">
             <div className="flex items-center justify-between">
-                <Dictionary className="text-grey-500" width={28} height={32} />
+                <Link
+                    href="/"
+                    className="dark:focus-ring-purple-500 rounded-sm outline-none focus:ring-1 focus:ring-purple-500 focus:ring-offset-4 dark:focus:ring-offset-grey-900"
+                >
+                    <Dictionary className="text-grey-500" width={28} height={32} />
+                </Link>
 
                 <section className="flex items-center gap-4">
                     <DropdownMenu>
