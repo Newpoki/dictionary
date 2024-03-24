28b26773e5e6dd852f99f12da183f4e6359032b3
Jason Savelli <jason.savelli@mention.com>
2024-03-24 15:18:49 +0100

    fix: remove useless transition color on input

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -15,7 +15,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             <div className="flex flex-col gap-2 text-[16px] font-bold leading-[17px] md:text-h-s">
                 <label
                     className={cn(
-                        'flex w-full items-center gap-4 rounded-2xl bg-grey-100 px-6 pb-[15px] pt-4 transition-colors focus-within:ring-1 focus-within:ring-purple-500 focus-visible:outline-none has-[:disabled]:cursor-not-allowed has-[:disabled]:opacity-50 dark:bg-grey-800 dark:focus-within:ring-purple-500 md:py-5',
+                        'flex w-full items-center gap-4 rounded-2xl bg-grey-100 px-6 pb-[15px] pt-4 focus-within:ring-1 focus-within:ring-purple-500 focus-visible:outline-none has-[:disabled]:cursor-not-allowed has-[:disabled]:opacity-50 dark:bg-grey-800 dark:focus-within:ring-purple-500 md:py-5',
                         {
                             'ring-1 ring-red-500 focus-within:ring-red-500 dark:ring-red-500 dark:focus-within:ring-red-500':
                                 hasError,
